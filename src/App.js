15d79f05a0b3e5ac00fa2bf51636a3429ff212cc
Ifrat Binte <ifratbinte@gmail.com>
2022-10-23 11:29:36 +0600

    initial c ommit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="row">
+        <div className="col-6">
+          <div className="text-center">Hello World !</div>
+        </div>
+        <div className="col-6">
+          <div className="text-center">Hello World !</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
